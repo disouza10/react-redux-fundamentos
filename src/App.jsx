0abfb5168d8e640857ada6176560929b2d90f13f
Diego Souza <diegoshowza93@gmail.com>
2023-04-10 22:10:48 -0300

    map and map challenge

--- src/App.jsx
+++ src/App.jsx
@@ -8,12 +8,22 @@ import Fragmento from './components/basicos/Fragmento'
 import Aleatorio from './components/basicos/Aleatorio'
 import Familia from './components/basicos/Familia'
 import FamiliaMembro from './components/basicos/FamiliaMembro'
+import ListaAlunos from './components/repeticao/ListaAlunos'
+import TabelaProdutos from './components/repeticao/TabelaProdutos'
 
 export default () =>
   <div className='app'>
     <h1>Fundamentos React</h1>
 
     <div className='cards'>
+      <Card titulo='#07 - Desafio Repetição' color='#3A9ADA'>
+        <TabelaProdutos />
+      </Card>
+
+      <Card titulo='#06 - Repetição' color='#FF4C65'>
+        <ListaAlunos />
+      </Card>
+
       <Card titulo='#05 - Componente com Filhos' color='#00C8F8'>
         <Familia sobrenome='Santos'>
           <FamiliaMembro nome='Diego' />
---
+++ src/data/produtos.js
@@ -0,0 +1,14 @@
+const produtos = [
+  { id: 1, nome: 'Lorem', preco: 10.0 },
+  { id: 2, nome: 'ipsum', preco: 15.5 },
+  { id: 3, nome: 'dolor', preco: 20.99 },
+  { id: 4, nome: 'sit', preco: 5.75 },
+  { id: 5, nome: 'amet', preco: 8.99 },
+  { id: 6, nome: 'consectetur', preco: 12.5 },
+  { id: 7, nome: 'adipiscing', preco: 18.0 },
+  { id: 8, nome: 'elit', preco: 22.75 },
+  { id: 9, nome: 'Duis', preco: 7.25 },
+  { id: 10, nome: 'iaculis', preco: 16.5 }
+]
+
+export default produtos
